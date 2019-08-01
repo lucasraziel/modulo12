8d455bad62f6329b1d89ceee02895615afb61132
root <root@DESKTOP-77RQ0M5.localdomain>
2019-08-01 12:33:19 -0300

    aula3 - Testando Formulario

--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -4,18 +4,32 @@ import React, { useState } from 'react';
 
 export default function TechList() {
 	const [techs, setTechs] = useState([]);
+	const [newTech, setNewTech] = useState('');
 
 	function handleAddTech() {
-		setTechs([...techs, 'Node.js']);
+		if (newTech) {
+			setTechs([...techs, newTech]);
+		}
+		setNewTech('');
 	}
+
 	return (
 		<div>
-			<ul data-testid="tech-list">
-				{techs.map(tech => (
-					<li key={tech}>{tech}</li>
-				))}
-			</ul>
-			<button onClick={handleAddTech}>Adicionar</button>
+			<form data-testid="tech-form" onSubmit={handleAddTech}>
+				<ul data-testid="tech-list">
+					{techs.map(tech => (
+						<li key={tech}>{tech}</li>
+					))}
+				</ul>
+
+				<label htmlFor="tech">Tech</label>
+				<input
+					id="tech"
+					value={newTech}
+					onChange={e => setNewTech(e.target.value)}
+				/>
+				<button type="submit">Adicionar</button>
+			</form>
 		</div>
 	);
 }
